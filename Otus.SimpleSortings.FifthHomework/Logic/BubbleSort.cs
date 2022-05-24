2989b019be6a7a3ef97c7d22687487eebabb9ed4
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-05-24 22:36:55 +0300

    implemented bubble sorting

---
+++ Otus.SimpleSortings.FifthHomework/Logic/BubbleSort.cs
@@ -0,0 +1,23 @@
+﻿namespace Otus.SimpleSortings.FifthHomework.Logic
+{
+    public class BubbleSort
+    {
+        public int[] Run(int[] array)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                for (var j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        var tmp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = tmp;
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
---
+++ Tests/Tests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using Otus.SimpleSortings.FifthHomework.Logic;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private int[] _initialArray = {3, 2, 10, 1, 5};
+        private int[] _sortedArray = {1, 2, 3, 5, 10};
+
+       
+        [Test]
+        public void Can_Sort_Via_Bubble_Sorting()
+        {
+            var bubbleSorter = new BubbleSort();
+
+            var sortedArray = bubbleSorter.Run(_initialArray);
+
+            CollectionAssert.AreEqual(_sortedArray, sortedArray);
+        }
+    }
+}
